461a2f6575112d54bd1225b3bb389824503d8542
GiuseppeArgentieri <giuseppe.arge24@outlook.it>
2023-12-23 11:32:15 +0100

    creazione primi componenti in angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  userid = 'Nicola'
-  password = ''
-  constructor() { }
+  userid: string = 'Nicola'
+  password: string = ''
+  autenticato: boolean = true;
+  errMsg: string = "Spiacente, la userid e/o la password sono errati";
+  
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
 
   gestAut(){
     console.log(this.userid);
+    if(this.userid === "Nicola" && this.password=="123_stella"){
+      this.route.navigate(['welcome', this.userid]);
+    }
+    else{
+      this.autenticato = false;
+    }
   }
 
 }
