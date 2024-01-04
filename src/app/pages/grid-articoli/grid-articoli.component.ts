a8c73dccae25c2e6569477b1dd06d2eb81b93beb
GiuseppeArgentieri <giuseppe.arge24@outlook.it>
2024-01-04 23:19:15 +0100

    code refactoring e aggiunta funzionalità di eliminazione in articoli grid

---
+++ src/app/pages/grid-articoli/grid-articoli.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { IArticoli } from 'src/app/models/articoli';
+import { ArticoliService } from 'src/services/articoli.service';
+
+@Component({
+  selector: 'app-grid-articoli',
+  templateUrl: './grid-articoli.component.html',
+  styleUrls: ['./grid-articoli.component.css']
+})
+export class GridArticoliComponent implements OnInit {
+  articoli$: IArticoli[] = [];
+  constructor(private articoliService: ArticoliService) { }
+
+  ngOnInit(): void {
+    this.articoli$ = this.articoliService.getArticoli();
+    console.log(this.articoli$);
+  }
+  handleEdit = (codart: string) => {
+    console.log("hai cliccati edit relativo al codice " + codart);
+  }
+  handleDelete = (codart: string) => {
+    console.log("hai cliccati delete relativo al codice " + codart);
+    this.articoli$.splice(this.articoli$.findIndex(x => x.codart === codart), 1);
+    console.log(this.articoli$);
+  }
+}
